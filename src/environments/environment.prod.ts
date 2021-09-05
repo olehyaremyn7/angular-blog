beb7dc775b7778615ce3b027d1174d78c43b8bc8
Oleh <58546051+olehyaremyn7@users.noreply.github.com>
2021-09-05 15:54:42 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: true,
-  API_KEY: 'AIzaSyBpHDV5qAveGteBLX-lmI1dTxM5_DggrHg',
-  fireBaseURL: 'https://angular-blog-app-f675d.firebaseio.com'
+  API_KEY: '',
+  fireBaseURL: ''
 };
